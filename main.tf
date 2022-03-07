6ca2c677ffea17b2b0fffbdfa2b54ee0e66b7e75
ville-petteri <ville-petteri.saily@live.com>
2022-03-07 11:36:06 +0200

    tagit ja vm lisätty

--- main.tf
+++ main.tf
@@ -17,6 +17,18 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "villepetteriterraform"
-  location = "westus2"
+      name     = "villepetteriterraform"
+      location = "westus2"
+
+      tags = {
+       Environment = "Terraform Getting Started"
+       Team = "DevOps"
+      }
+}
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+    name                = "villepetteriTFVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "westus2"
+    resource_group_name = azurerm_resource_group.rg.name
 }
